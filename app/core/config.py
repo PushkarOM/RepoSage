bf97a3dc29334560ea61a442e56729336a8bc61d
PushkarOM <pushkarchaturvedi42@gmail.com>
2026-07-05 11:04:11 +0530

    fix: Fallback when llm api reaches limit, config update now uses model name from env

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,7 @@ class Settings(BaseSettings):
     embedding_provider: str = "local"
     embedding_model_local: str = "sentence-transformers/all-MiniLM-L6-v2"
     google_api_key: str | None = None
+    google_model_name: str = "gemini-2.5-flash-lite"
     chroma_persist_dir: str = "./chroma_db"
     clone_dir: str = "./cloned_repos"
 
